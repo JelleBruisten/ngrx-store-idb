3a1aa4b47a450808fa112e3d6422cf8b0c1d1663
Jelle Bruisten <jelle_bruisten@hotmail.com>
2023-03-06 21:08:51 +0100

    feat: added some options to play with

--- src/app/idb/idb-store.config.ts
+++ src/app/idb/idb-store.config.ts
@@ -1,14 +1,40 @@
 import { InjectionToken } from "@angular/core"
 
+export type IdbStoreSynchronize = 'init' | 'visibilityChange' | 'broadcastChannelNotify';
+
 export interface IdbStoreConfig {
   /**
    * Not recommended, might cause weird behavior
    */
   synchronizeWhenDocumentHidden: boolean;
+
+  /**
+   * When to perform a synchronize
+   */
+  readIdbOn: IdbStoreSynchronize[],
+
+  /**
+   * Which channel name to use, defaults to 'IdbStoreChannel'
+   */
+  broadcastChannelName: string;  
+  broadcastChannelNotifyThrottleTime: number;
+  broadcastChannelReceiveDebounceTime: number;
+  skipMessagesWhileHidden: boolean;
+  skipNotifyWhileHidden: boolean;
 }
 
 export const idbStoreConfig = new InjectionToken<IdbStoreConfig>('IdbStoreConfig');
 
 export const defaultConfig: IdbStoreConfig = {
-  synchronizeWhenDocumentHidden: false
+  synchronizeWhenDocumentHidden: false,
+  broadcastChannelName: 'IdbStoreChannel',
+  readIdbOn: [
+    'init',
+    'visibilityChange',
+    'broadcastChannelNotify'
+  ],
+  broadcastChannelNotifyThrottleTime: 500,
+  broadcastChannelReceiveDebounceTime: 500,
+  skipMessagesWhileHidden: false,
+  skipNotifyWhileHidden: true
 };
--- src/app/idb/idb-store.effect.ts
+++ src/app/idb/idb-store.effect.ts
@@ -1,11 +1,11 @@
 import { Inject, Injectable } from "@angular/core";
-import { Actions, OnInitEffects, createEffect, ofType } from "@ngrx/effects";
-import { Action, ActionCreator, ReducerManager, UPDATE } from "@ngrx/store";
-import { initAction, synchronizeAction } from './idb-store.actions';
-import { EMPTY, Observable, filter, fromEvent, switchMap, tap } from "rxjs";
-import { IdbStoreConfig, idbStoreConfig } from "./idb-store.config";
+import { Actions, concatLatestFrom, createEffect } from "@ngrx/effects";
+import { Action, ReducerManager } from "@ngrx/store";
+import { synchronizeAction } from './idb-store.actions';
+import { EMPTY, Observable, debounceTime, filter, fromEvent, map, skip, startWith, switchMap, tap, throttleTime } from "rxjs";
 import { get as idbGet, keys as idbKeys } from "idb-keyval";
 import { DOCUMENT } from "@angular/common";
+import { IdbStoreConfig, idbStoreConfig } from "./idb-store.config";
 
 @Injectable()
 export class IdbStoreEffect {
@@ -13,24 +13,62 @@ export class IdbStoreEffect {
   // whenever a new reducer has been added, synchronize 
   reducersChanged$ = createEffect(() => {
     return this.reducerManager.pipe(
+      skip(this.config.readIdbOn.includes('init') ? 0 : 1),
       switchMap(() => this.createSynchronizeAction())
     )
   })
 
+  pageVisibility$ = fromEvent(this.document, 'visibilitychange').pipe(map((event) => !this.document.hidden), startWith(!this.document.hidden));
+
   // whenever we go from unfocused to focused synchronize
-  visibilityChange$ = createEffect(() => fromEvent(this.document, 'visibilitychange').pipe(
-    switchMap(() => {
-      if(this.document.hidden) {
+  visibilityChange$ = createEffect(() => this.pageVisibility$.pipe(
+    filter(() => this.config.readIdbOn.includes('visibilityChange')),
+    switchMap((visible) => {
+      if(!visible) {
         return EMPTY;
       }
       return this.createSynchronizeAction();
     })
-  ));
+  ));  
+
+  channel = new BroadcastChannel(this.config.broadcastChannelName);
+  channelNotified$ = createEffect(() => fromEvent(this.channel, 'message').pipe(
+    filter(() => this.config.readIdbOn.includes('broadcastChannelNotify')),
+    tap(x => console.log(x)),
+    debounceTime(this.config.broadcastChannelReceiveDebounceTime),  
+    tap(x => console.log(x)),
+    concatLatestFrom(() => this.pageVisibility$),
+    switchMap(([event, visible]) => {
+      if(this.config.skipMessagesWhileHidden && !visible) {
+        return EMPTY;
+      }
+      return this.createSynchronizeAction();
+    })
+  ));  
+
+  notifyOtherTabs$ = createEffect(() => this.actionSubject.pipe(    
+    filter(() => this.config.readIdbOn.includes('broadcastChannelNotify')),
+    filter((action) => !action.type.startsWith('@ngrx') && action.type !== synchronizeAction.type),
+    throttleTime(this.config.broadcastChannelNotifyThrottleTime),
+    concatLatestFrom(() => this.pageVisibility$),
+    tap(([event, visible]) => {
+      if(this.config.skipNotifyWhileHidden && !visible) {
+        return;
+      }
+      this.channel.postMessage(event);      
+    })
+  ), {
+    dispatch: false
+  });  
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    private reducerManager: ReducerManager
-  ){}
+    private reducerManager: ReducerManager,
+    private actionSubject: Actions,
+    @Inject(idbStoreConfig) private config: IdbStoreConfig
+  ){
+
+  }
 
   createSynchronizeAction() {
     return new Observable<Action>((subscriber) => {
